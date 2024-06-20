d3112fd22966bad1a0a5b270c902ac57cc9448f4
SriramKalki <sriramkalki@hotmail.com>
2024-06-19 23:00:42 -0500

    Flask-Migrate

--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,22 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash
+from .models import Post, db
+from .forms import PostForm
+from datetime import datetime
 
 main = Blueprint('main', __name__)
 
-posts = []
-
 @main.route('/')
 def index():
+    posts = Post.query.all()
     return render_template('index.html', posts=posts)
 
-@main.route('/create', methods=['GET', 'POST'])
-def create_post():
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-        posts.append({'title': title, 'content': content})
+@main.route('/new', methods=['GET', 'POST'])
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, date_posted=datetime.utcnow())
+        db.session.add(post)
+        db.session.commit()
+        flash('Post created successfully!', 'success')
         return redirect(url_for('main.index'))
-    return render_template('create_post.html')
-
-@main.route('/delete/<int:post_id>')
-def delete_post(post_id):
-    if 0 <= post_id < len(posts):
-        posts.pop(post_id)
-    return redirect(url_for('main.index'))
+    return render_template('new_post.html', form=form)
