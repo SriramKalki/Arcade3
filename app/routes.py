c9bafa4c62f8ed3a5bd35e82942ff65b0724e46d
SriramKalki <sriramkalki@hotmail.com>
2024-06-19 20:21:25 -0500

    More work

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 main = Blueprint('main', __name__)
 
+posts = []
+
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
+
+@main.route('/create', methods=['GET', 'POST'])
+def create_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        posts.append({'title': title, 'content': content})
+        return redirect(url_for('main.index'))
+    return render_template('create_post.html')
